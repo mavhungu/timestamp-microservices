8e74956138cfe4f173e9e0f6f09be2f3da9970c0
ronewa <ronewamavhungu@gmail.com>
2021-12-03 17:58:24 +0200

    adding router and controller home file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const home = require('./router/home')
+
+app.get('/',home)
+
 
 app.listen(port,()=>{
     console.log(`Server is running at port ${port}`)
