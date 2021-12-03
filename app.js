19026856d2955dcf7923dda325b5b94e1fa30059
ronewa <ronewamavhungu@gmail.com>
2021-12-03 18:06:21 +0200

    adding templates folders

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
 const port = process.env.PORT || 3000
 
 const home = require('./router/home')
+
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname,'templates/views'))
+hbs.registerPartial(path.join(__dirname,'templates/partials'))
 
 app.get('/',home)
 
