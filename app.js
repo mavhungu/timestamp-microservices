4d6d9f023836b9ea6e8bb8254addc38165559a60
ronewa <ronewamavhungu@gmail.com>
2021-12-04 05:15:20 +0200

    adding Author, description & scripts (package)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const hbs = require('hbs')
 const port = process.env.PORT || 3000
 
@@ -9,7 +10,7 @@ const timestampDateRouter = require('./router/timestampdateRouter')
 
 app.set('view engine', 'hbs')
 app.set('views',path.join(__dirname,'templates/views'))
-hbs.registerPartial(path.join(__dirname,'templates/partials'))
+hbs.registerPartials(path.join(__dirname,'templates/partials'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
