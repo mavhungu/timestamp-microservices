358700d8a0a1d8aafc0f9731cd50be6c10df397c
ronewa <ronewamavhungu@gmail.com>
2021-12-04 05:04:30 +0200

    adding index

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 const home = async (req, res)=>{
-    res.render('index',{})
+    res.render('index',{
+        'title': 'Index-page',
+        'home': 'Timestamp Microservice'
+    })
 }
 
 module.exports= home
